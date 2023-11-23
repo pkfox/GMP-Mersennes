8561d6d1f4660784b0785f85bf54ba2e69a99189
pkfox <pete@pjksolutions.com>
2023-11-23 17:04:01 +0000

    Finally got the postgres connection working as a class member

--- PGMersenne.h
+++ PGMersenne.h
@@ -11,12 +11,11 @@ class PGMersenne
 public:
 	PGMersenne(int primepower, std::string mersenneprime,int PrimeProbability);
 	std::size_t EditMersenne();
-	
 private:
 	int Primepower;
 	std::string Mersenneprime;
 	std::string ConnectionString = "host=nuc port=5432 dbname=commands user=postgres password=Giraffes09";
-    int PrimeProbability;
+        int PrimeProbability;
 	pqxx::connection PGConnection = pqxx::connection(this->ConnectionString);
 };
 #endif
