db6885c96837af771bb8e2f97018e21cc91030f8
Pete Kane <pete@pjksolutions.com>
2023-12-16 17:53:31 +0000

    Last push from Dell

--- main.cpp
+++ main.cpp
@@ -8,11 +8,23 @@
 #include <chrono>
 #include <algorithm>
 #include "Utils.h"
+#include <algorithm>
 
 using namespace pqxx;
 
-	int main(int argc, char** argv)
+	int main(int argc, char* argv[])
 	{
+		std::vector<std::string> Args(argv + 1, argv + argc);
+		
+		bool SkipPrimalityTest = std::find(Args.begin(), Args.end(), "-skiptest") != Args.end();
+		bool GiveFeedback = std::find(Args.begin(), Args.end(), "-givefeedback") != Args.end();
+		std::stringstream ss;
+		ss << "Running mersenne search with these parameters\n";
+		ss << "Skip primality test = " << (SkipPrimalityTest ? "true" : "false") << "\n";
+		ss << "Give feedback = " << (GiveFeedback ? "true" : "false") << "\n";
+		Utils::PrintMessage(ss.str());
+
+		std::copy(argv, argv + argc, std::ostream_iterator<char*>(std::cout, "\n"));
 		mpir_ui StartRange;
 		mpir_ui EndRange;
 		std::vector<Pow2Result> Results;
@@ -28,7 +40,7 @@ using namespace pqxx;
 			StartRange = Primes[i] - 1;
 			EndRange = Primes[i];
 			Results.clear();
-			MersennePrimes mp(StartRange, EndRange, argc > 3);
+			MersennePrimes mp(StartRange, EndRange, GiveFeedback,SkipPrimalityTest);
 			mp.GenerateListOfMersennes();
 			Results = mp.GetResults();
 
