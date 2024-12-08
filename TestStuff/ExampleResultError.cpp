5e61ffc8739f4cca28062d696730f8414a7d4f92
Pete Kane <pete@pjksolutions.com>
2024-12-08 10:58:13 +0000

    Update ExampleResultError.cpp

--- TestStuff/ExampleResultError.cpp
+++ TestStuff/ExampleResultError.cpp
@@ -9,7 +9,7 @@ int main()
 {
     try
     {
-        pqxx::connection c("host=nuc port=5432 dbname=commands user=postgres password=Giraffes09");
+        pqxx::connection c("host=beelink port=5432 dbname=commands user=postgres password=Giraffes09");
         pqxx::work w(c);
         w.exec("select getprimes()").for_each([] (int prime) { AddPrime(prime); });
         std::cout << Primes.size() << " primes returned from database\n";
