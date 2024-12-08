21686265a528ce3ca5a80e6866f9deba3e2552ca
pkfox <pete@pjksolutions.com>
2024-12-08 19:16:20 +0000

    Got it building on Dell as well as surface stdC++17 and SDK 10.0.22621.0

--- PGMersenne.cpp
+++ PGMersenne.cpp
@@ -59,7 +59,7 @@ namespace pjk
 			return;
 		}
 
-		this->PGTransaction.exec_params("select getprimes($1)",25)
+		this->PGTransaction.exec_params("select getprimes($1)",52)
 		.for_each([&Primes](int prime) { Primes.push_back(prime); });
 		this->PGTransaction.commit();
 	}
