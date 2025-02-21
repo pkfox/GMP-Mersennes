cf704e4f654be2ba7f2ec24613860ba0d66b4245
pkfox <pete@pjksolutions.com>
2025-02-21 11:43:23 +0000

    All seems to work on Linux and Windows

--- MersennePrime.cpp
+++ MersennePrime.cpp
@@ -5,18 +5,14 @@ MersennePrime::MersennePrime():Mersenne(0),MersenneResult(""),PrimeProbability(0
 
 }
 
-<<<<<<< HEAD
-MersennePrime::MersennePrime(int Mersenne, std::string MersenneResult,
- int PrimeProbability,std::string Duration):MersennePrime()
-=======
-MersennePrime::MersennePrime(int Mersenne, std::string MersenneResult, int PrimeProbability):MersennePrime()
->>>>>>> a9eb77f3be0f0d51cbdb9807717e538655eb21e4
+
+MersennePrime::MersennePrime(int Mersenne, std::string MersenneResult,int PrimeProbability, std::string Duration) :MersennePrime()
 {
-	this->Mersenne = Mersenne;
-	this->MersenneResult = MersenneResult;
-	this->PrimeProbability = PrimeProbability;
-	this->PrimeProbabilityText = PrimeStatus::GetStatusMessage(this->PrimeProbability);
-	this->Duration = Duration;
+    this->Mersenne = Mersenne;
+    this->MersenneResult = MersenneResult;
+    this->PrimeProbability = PrimeProbability;
+    this->PrimeProbabilityText = PrimeStatus::GetStatusMessage(this->PrimeProbability);
+    this->Duration = Duration;
 }
 
 std::string MersennePrime::GetJSON()
--- MersennePrime.h
+++ MersennePrime.h
@@ -5,40 +5,20 @@
 #include <rapidjson/writer.h>
 #include "PrimeStatus.h"
 
-<<<<<<< HEAD
-
-//namespace pjk
-//{
-	// Storage class for a Mersenne prime
-	class MersennePrime
-	{
-	public:
-		MersennePrime();
-		MersennePrime(int Mersenne,
-		std::string MersenneResult,
-		int PrimeProbability,std::string Duration);
-
-		std::string GetJSON();
-		int Mersenne;
-		std::string MersenneResult;
-		int PrimeProbability;
-		std::string PrimeProbabilityText;
-		std::string Duration;
-	};
-//}
-=======
 // Storage class for a Mersenne prime
 class MersennePrime
 {
 public:
 	MersennePrime();
-	MersennePrime(int Mersenne, std::string MersenneResult, int PrimeProbability);
+	MersennePrime(int Mersenne,
+		std::string MersenneResult,
+		int PrimeProbability, 
+		std::string Duration);
+
 	std::string GetJSON();
 	int Mersenne;
 	std::string MersenneResult;
 	int PrimeProbability;
 	std::string PrimeProbabilityText;
+	std::string Duration;
 };
-
->>>>>>> a9eb77f3be0f0d51cbdb9807717e538655eb21e4
-
